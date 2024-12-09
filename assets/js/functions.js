54708bf5db7d31bf22ece9796c5501613348f87a
Turmenas <161265237+Turmenas@users.noreply.github.com>
2024-12-09 14:49:05 +0200

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -27,3 +27,19 @@ const scrollToggle = document.getElementById('scroll-toggle');
 scrollToggle.addEventListener("click", () => {
     window.scrollTo(0, 0)
 });
+
+//Clock
+
+function updateClock() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
